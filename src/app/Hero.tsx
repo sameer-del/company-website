28d1b10e69e3ff46235e753b7ad3d7a8763d296a
Sameer Ahamed <122287404+sameer-del@users.noreply.github.com>
2024-12-19 12:28:03 +0530

    adjusting the content

--- src/app/Hero.tsx
+++ src/app/Hero.tsx
@@ -4,30 +4,30 @@ import Link from "next/link";
 
 export default function Hero() {
   return (
-    <section className="  pt-5">
+    <section className="pt-5">
       {/* hero Page */}
 
       <div className="relative flex flex-col justify-center items-center h-full w-full py-10">
-        <div className=" text-black w-full flex justify-center items-center flex-col  px-[200px]">
+        <div className=" text-black w-full flex justify-center items-center flex-col  lg:px-[200px]">
           {/* styling elements */}
-          <div className="absolute top-10 left-[100px]">
+          <div className="hidden lg:block absolute top-10 left-[100px]">
             <img src="/assests/squiggle-2.svg" alt="" />
             <img src="/assests/lightning.svg" alt="" />
           </div>
-          <div className="absolute top-0 right-[100px]">
+          <div className="hidden lg:block  absolute top-0 right-[100px]">
             <img src="/assests/squiggle-2.svg" alt="" />
             <img src="/assests/lightning.svg" alt="" />
           </div>
           {/* styling elements */}
 
-          <h1 className="text-[70px] text-center font-bold capitalize font-sans">
+          <h1 className="lg:text-[70px] text-[40px]  text-center font-bold capitalize font-sans">
             Innovating the Future with <br />
             <span className=" inline-block ">
               <Typeanimation />
             </span>
             Software Solutions
           </h1>
-          <p className="text-[19px] mt-5 font-mono capitalize text-center max-w-[85%]">
+          <p className="text-[19px] mt-5 font-mono capitalize text-center  lg:max-w-[85%]">
             At Ruduo Technologies, we bring your ideas to life with innovative
             software solutions tailored to your unique needs.
           </p>
@@ -65,7 +65,7 @@ export default function Hero() {
             className="rounded-[15px] shadow-2xl "
           />
         </div>
-        <div className="w-full   px-[200px] py-10">
+        <div className="w-full   lg:px-[200px] py-10">
           <p className="text-[#A880FF] text-center font-serif">
             More about Spike Teamspace
           </p>
@@ -74,9 +74,11 @@ export default function Hero() {
           </p>
         </div>
       </div>
+
+      {/* product grid */}
       <div className="w-full   mt-5">
-        <div className=" grid grid-cols-3 gap-[50px] px-[100px] py-10 ">
-          <div className="w-[430px] h-[485px] bg-[#d8d8d8] px-[20px] py-3 shadow-2xl   rounded-[15px] flex justify-center items-center flex-col">
+        <div className="w-full  grid grid-cols-1 lg:grid-cols-3 items-center gap-[50px] lg:px-[100px] px-5 py-10 ">
+          <div className="lg:w-[430px] w-auto   h-[485px] bg-[#d8d8d8] lg:px-[20px] py-3 shadow-2xl   rounded-[15px] flex justify-center items-center flex-col">
             <div className="flex-1  w-full   ">
               <Image
                 src="/assests/pm-card-1.webp"
@@ -88,18 +90,18 @@ export default function Hero() {
             </div>
             <div className="h-[210px]  w-full ">
               <h1 className="text-center font-sans uppercase font-bold text-[23px]">
-                sales Marketing
+                Workflow Engine
               </h1>
               <p className="max-w-[85%] mx-auto py-2">
-                Lorem ipsum dolor, sit amet consectetur adipisicing elit.
-                Recusandae molestias ut iste! Adipisci non recusandae
-                reiciendis! Doloremque accusantium laborum, maxime laboriosam
-                sapiente blanditiis. Alias maxime unde quia voluptatum optio
-                assumenda.
+                A Workflow Engine is a powerful software tool designed to
+                automate, streamline, and manage complex business processes. It
+                ensures tasks are executed in a logical, sequential manner while
+                coordinating between people, systems, and data.
               </p>
             </div>
           </div>
-          <div className="w-[430px] h-[485px] bg-[#d8d8d8] px-[20px] py-3 shadow-2xl    rounded-[15px] flex justify-center items-center flex-col">
+          {/* grid-2 */}
+          <div className="lg:w-[430px] w-auto  h-[485px] bg-[#d8d8d8] px-[20px] py-3 shadow-2xl    rounded-[15px] flex justify-center items-center flex-col">
             <div className="flex-1  w-full  ">
               <Image
                 src="/assests/pm-card-2.webp"
@@ -111,18 +113,18 @@ export default function Hero() {
             </div>
             <div className="h-[210px]  w-full ">
               <h1 className="text-center font-sans uppercase font-bold text-[23px]">
-                sales Marketing
+                Rule Engine
               </h1>
               <p className="max-w-[85%] mx-auto py-2">
-                Lorem ipsum dolor, sit amet consectetur adipisicing elit.
-                Recusandae molestias ut iste! Adipisci non recusandae
-                reiciendis! Doloremque accusantium laborum, maxime laboriosam
-                sapiente blanditiis. Alias maxime unde quia voluptatum optio
-                assumenda.
+                A Rule Engine is a software system that evaluates and executes
+                predefined rules or conditions to make automated decisions. It
+                separates the logic of decision-making from the main
+                application.
               </p>
             </div>
           </div>
-          <div className="w-[430px] h-[485px] bg-[#d8d8d8] px-[20px] py-3  shadow-2xl   rounded-[15px] flex justify-center items-center flex-col">
+          {/* grid-3 */}
+          <div className="lg:w-[430px] w-auto  h-[485px] bg-[#d8d8d8] px-[20px] py-3  shadow-2xl   rounded-[15px] flex justify-center items-center flex-col">
             <div className="flex-1  w-full  ">
               <Image
                 src="/assests/pm-card-3.webp"
@@ -134,18 +136,18 @@ export default function Hero() {
             </div>
             <div className="h-[210px]  w-full ">
               <h1 className="text-center font-sans uppercase font-bold text-[23px]">
-                sales Marketing
+                Bug Tracker
               </h1>
               <p className="max-w-[85%] mx-auto py-2">
-                Lorem ipsum dolor, sit amet consectetur adipisicing elit.
-                Recusandae molestias ut iste! Adipisci non recusandae
-                reiciendis! Doloremque accusantium laborum, maxime laboriosam
-                sapiente blanditiis. Alias maxime unde quia voluptatum optio
-                assumenda.
+                A Bug Tracker is a tool or software used by development teams to
+                identify, document, prioritize, and resolve software bugs or
+                issues throughout the development lifecycle. It centralizes bug
+                reporting and tracking.
               </p>
             </div>
           </div>
         </div>
+        {/* grid end */}
         <div className="flex justify-center items-center">
           <button className="w-[260px] h-[54px] bg-[#A980FE]  text-[18px] tracking-[1px] mt-3  font-sans text-white rounded-[10px]">
             View All our products
@@ -154,9 +156,9 @@ export default function Hero() {
       </div>
 
       {/* another section */}
-      <div className="h-[60vh] w-full  mt-[50px] flex justify-between items-center px-[100px]">
+      <div className="h-full w-full  mt-[50px] flex justify-between items-center flex-col gap-5 lg:flex-row lg:px-[100px]">
         <div className=" w-full h-full flex justify-center flex-col">
-          <h1 className="text-[45px] ">
+          <h1 className="text-[45px] lg:text-start text-center px-2 lg:px-0">
             Try our Product we have a <br /> high quality software with full
             <br />
             time support
@@ -170,7 +172,7 @@ export default function Hero() {
             </button>
           </div>
         </div>
-        <div className="  w-full h-full flex justify-center items-center flex-col">
+        <div className="  w-full h-full flex justify-center items-center flex-col px-2">
           <Image
             src="/images/software-developer1.jpg"
             width={450}
@@ -181,12 +183,12 @@ export default function Hero() {
         </div>
       </div>
       {/* another section */}
-      <div className="bg-[#a880ff]   w-full h-[50vh] mt-5 flex justify-center items-center">
+      <div className="bg-[#a880ff]   w-full lg:h-[50vh] h-full py-5 mt-5 flex justify-center items-center">
         <div className=" h-full max-w-[85%] flex justify-center flex-col items-center gap-5">
           <h1 className="text-[40px] text-white font-bold  capitalize text-center">
             questions? we'll put your right <br /> path.
           </h1>
-          <p className="max-w-[75%] mx-auto text-center text-white font-medium capitalize text-[19px]">
+          <p className="lg:max-w-[75%] mx-auto text-center text-white font-medium capitalize text-[19px]">
             Have questions, need assistance, or ready to start your next big
             project? We're here to help! Get in touch with our expert team to
             discuss your software development needs, share your vision, or
